2ef4da15d5bd5c86157b276cad4ec738937d42b3
Prakarsh Srivastava <Prakarsh_Srivastava@epam.com>
2022-04-22 14:41:34 +0530

    UnCommented

--- Program.cs
+++ Program.cs
@@ -14,7 +14,6 @@ namespace BooksDeserialisation
     {
         public static void DeSerialiseData()
         {
-
             XmlDocument doc = new XmlDocument();
             doc.Load(@"C:\Users\Prakarsh_Srivastava\OneDrive - EPAM\BooksDeserialisation\books.xml");
             Catalog obj;
@@ -25,7 +24,7 @@ namespace BooksDeserialisation
                     XmlSerializer serializer = new XmlSerializer(typeof(Catalog));
                     obj = (Catalog)serializer.Deserialize(reader);
                     var sb = new System.Text.StringBuilder();
-                    for (int i =0; i<obj.books.Count;i++ )
+                    for(int i =0; i<obj.books.Count;i++ )
                     {
                         sb.Append("Book ID : " + obj.books[i].id);
                         sb.Append("Book Title: " + obj.books[i].title);
@@ -33,23 +32,12 @@ namespace BooksDeserialisation
                         sb.Append("Genre: " + obj.books[i].genre);
                         sb.Append("Publisher: "+obj.books[i].publisher);
                         sb.AppendLine("Publish Date: " + obj.books[i].publish_date);
-                        /*Console.WriteLine(obj.books[i].genre);*/
-                        /*                        Console.Write("Book ID : "+obj.books[i].id+" "+"Book Title: " +obj.books[i].title + " " +"Book Author "+ obj.books[i].author
-                                                    +" "+"Genre: "+ obj.books[i].genre+" "+
-                        "Publisher: "+obj.books[i].publisher+" "+"Publish Data: "+ obj.books[i].publish_date);*/
-
-
-
-
-
                     }
                     Console.WriteLine(sb.ToString());
-
                 }
             }
             string json = JsonConvert.SerializeObject(obj);
             File.WriteAllText(@"C:\Users\Prakarsh_Srivastava\OneDrive - EPAM\BooksDeserialisation\Jbooks.json", json);
-
         }
         static void Main(string[] args)
         {
